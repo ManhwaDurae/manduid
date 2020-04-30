6ff29a06b518cedc0c057cbfdfa67ec8af30eb10
LiteHell <litehell@litehell.info>
2020-04-30 22:57:13 +0900

    Add xlsx save feature

--- src/routers/admin/roll.ts
+++ src/routers/admin/roll.ts
@@ -8,11 +8,16 @@ import bodyParser from 'koa-bodyparser';
 import { Member } from '../../models/Member';
 import { FindOptions, WhereOptions, Op, IncludeOptions } from 'sequelize';
 import { restrictByPermission } from './middleware';
+import xlsx from 'xlsx';
 
 let router = new Router<DefaultState, Context & ParameterizedContext>();
 
-router.get('/', restrictByPermission('roll.list'), async (ctx: Context) => {
-    let {schoolRegistration, query, query_type, rolls, limit, page} = ctx.request.query;
+const rollDisplayNames : {[index in RollType] : string} = {'AssociateMember' : '준회원', 'RegularMember' : '정회원', 'HonoraryMember' : '명예회원', 'Explusion' : '제적', 'PermanentExplusion' : '제명'}
+const schoolRegistrationDisplayNames : {[index in SchoolRegistration] : string} = {'Enrolled' : '재학', 'LeaveOfAbsence' : '휴학', 'Graduated' : '졸업', 'Expelled' : '제적'}
+
+router.get('/', restrictByPermission('roll.list'), async (ctx: Context) => { 
+
+    let {schoolRegistration, query, query_type, rolls, limit, page, saveAs} = ctx.request.query;
     let findOptions : FindOptions = {where: {}};
     let where : WhereOptions = {};
 
@@ -60,13 +65,44 @@ router.get('/', restrictByPermission('roll.list'), async (ctx: Context) => {
         query_type = null;
     }
     findOptions.where = where;
-    let count = await Roll.count(findOptions);
-    let pages = Math.ceil(count / limit),
-        offset = (page - 1) * limit;
-    findOptions.limit = limit;
-    findOptions.offset = offset;
-    let result = await Roll.findAll(findOptions);
-    await ctx.render('admin/roll', {pages, page, limit, roll: result, searchInfo : {query, query_type, rolls, schoolRegistration}});
+    if (saveAs === "excel") {
+        findOptions.include.push({model: ExecutiveType, required: false});
+        let result = await Roll.findAll(findOptions);
+        let xlsxData = [
+            ['저장 정보'], ['저장한 사람', (await ctx.user.$get('member')).name, '저장 일시', new Date(), '총 갯수', `${result.length}개`],
+            ['검색 조건'],[
+            '학적', schoolRegistration.length == 0 ? '(전체)' : schoolRegistration.map((i : string) => schoolRegistrationDisplayNames[<SchoolRegistration>i]),
+            '명부', rolls.length == 0 ? '(전체)' : rolls.map((i : string) => rollDisplayNames[<RollType>i]),
+            '검색어 종류', query_type || '(미지정)',
+            '검색어', query],
+            ['결과'],
+            ['명부', '이름', '학과', '학번', '생일', '전화번호', '직책', '계정 ID']
+        ];
+        for (let i of result) {
+            let user = await i.member.$get('user');
+            let executiveName = '';
+            if(i.executiveType) executiveName = i.executiveType.name;
+            else if (i.isPresident) executiveName = '회장';
+            xlsxData.push([rollDisplayNames[i.rollType], i.member.name, i.member.department, i.member.studentId, i.member.birthday, i.member.phoneNumber, executiveName, user ? user.id : ''])
+        }
+        let xlsxWorksheet = xlsx.utils.aoa_to_sheet(xlsxData);
+        let xlsxWorkbook = xlsx.utils.book_new();
+        xlsx.utils.book_append_sheet(xlsxWorkbook, xlsxWorksheet, '결과');
+        let xlsxFiledata : Buffer = xlsx.write(xlsxWorkbook, {bookType: 'xlsx', type: 'buffer'});
+
+        ctx.set('Content-Disposition', 'attachment; filename="download.xlsx"');
+        ctx.set('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+
+        ctx.body = xlsxFiledata;
+    } else {
+        let count = await Roll.count(findOptions);
+        let pages = Math.ceil(count / limit),
+            offset = (page - 1) * limit;
+        findOptions.limit = limit;
+        findOptions.offset = offset;
+        let result = await Roll.findAll(findOptions);
+        await ctx.render('admin/roll', {rollDisplayNames, schoolRegistrationDisplayNames, pages, page, limit, roll: result, searchInfo : {query, query_type, rolls, schoolRegistration}});
+    }
 });
 router.get('/new', restrictByPermission('roll.create'), async (ctx : Context) => {
     await ctx.render('admin/roll_fields', {add_member: true});
